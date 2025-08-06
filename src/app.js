3e8005a6a578e9ee7d01d4b4e57690be98dc9dba
vinit <vinitkumar93341@gmail.com>
2025-08-07 00:51:20 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const app = express();
 
-//query
-// http://localhost:7000/card?id=123&ui=vinit
-app.get("/card",(req,res)=>{
-  console.log(req.query);
-  res.send("hello from card")
+
+app.get("/admin/getUsers",(req,res)=>{
+  try{
+    //going for a db call
+
+    throw new Error("affafsd");
+    res.send("all users data sent");
+  }catch(err){
+    res.status(500).send("internal server error")
+  }
 })
 
-//for dynamic routing
-//http://localhost:7000/hello/123/90
-app.get("/hello/:id/:game",(req,res)=>{
-  console.log(req.params);                   // { id: '123', game: '90' }
-  res.send("hello bro is seeing params")
-})
 
-app.post("/user",(req,res)=>{
-  //saving data to db logic
-  //sending confirmation
-  res.send("data saved to db")
-})
 
 app.listen(7000, () => {
   console.log("app listening on port 7000");
