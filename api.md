8314bf3e80e40bb413b72d61a767127fdb15c088
vinit <vinitkumar93341@gmail.com>
2025-08-21 21:00:29 +0530

    feed

--- api.md
+++ api.md
@@ -12,4 +12,13 @@
 - GET /user/connections
 - GET /user/feed - get you the profiel of other users on platform
 
-status: ignored, interested, accepted, rejected
+Status: while swipping -> [ignored , interested]   judgement of request ->  [accepted , rejected ]
+
+
+/feed?page=1&limit=10 => 1-10 => .skip(0) & .limit(10)
+/feed?page=2?limit=10 => 11-20 => .skip(10) & .limit(10)
+/feed?page=3?limit=10 => 21-30 => .skip(20) & .limit(10)
+/feed?page=4?limit=10 => 31-40 => .skip(30) & .limit(10)
+
+//skip for page=1 => page-1*limit = 1- 1 * 10 = 0 ...so skip first 0 documents
+//skip for page=2 => 2-1*10 = 10 ...so skip first 10 documents
--- src/routes/user.js
+++ src/routes/user.js
@@ -7,10 +7,11 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
-const USER_SAFE_DATA = "firstName lastName phtoUrl age gender about skills"
+const USER_SAFE_DATA = "firstName lastName phtoUrl age gender about skills";
 
 //1 loggedin user pending requests
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
@@ -20,10 +21,7 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
       //connection request document
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate(
-      "fromUserId",
-      USER_SAFE_DATA
-    );
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.status(200).json({
       message: "data sent successfully",
@@ -43,32 +41,92 @@ userRouter.get("/user/total/connections", userAuth, async (req, res) => {
       $or: [
         {
           fromUserId: loggedInUser._id,
-          status: "accepted"
+          status: "accepted",
         },
         {
           toUserId: loggedInUser._id,
-          status:"accepted"
+          status: "accepted",
         },
       ],
-    }).populate("fromUserId",USER_SAFE_DATA).populate("toUserId",USER_SAFE_DATA)
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
     //now we dont want to show us
     //we need to filter out data where fromUserId or toUserId is not us
     const connections = data.map((c) => {
-        if(c.fromUserId._id.toString() === loggedInUser._id.toString()){
-            return c.toUserId;
-        }else{
-            return c.fromUserId
-        }
-    })
+      if (c.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return c.toUserId;
+      } else {
+        return c.fromUserId;
+      }
+    });
     res.status(200).json({
-        message:"all of your connections",
-        connections
-    })
+      message: "all of your connections",
+      connections,
+    });
   } catch (error) {
-    res.status(404).send("error ", error.message);
+    res.status(400).json({
+      message: error.message,
+    });
   }
 });
 
+//feed of the logged in user
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    //user should see the cards except
+    // - his own card
+    // - his connections
+    // - ignored people
+    // - already sent connection
+    // - basicaaalyyy all the people he has already interacted
+    const page = req.query.page || 1;
+    let limit = req.query.limit || 10;
+    limit = limit > 10 ? 10 : limit;
+    const skip = page - 1 * limit; //check api.md
 
+    const loggedInUser = req.user;
+    //now finding all the document where i was involved
+    const noFeed = await ConnectionRequest.find({
+      $or: [
+        {
+          fromUserId: loggedInUser._id,
+        },
+        {
+          toUserId: loggedInUser._id,
+        },
+      ],
+    }).select("fromUserId toUserId"); // no need of status , timestamps etc
+
+    //now creating a set of these people...who are not allowed in feed
+    const mySet = new Set(); // our own objectId and other user objectId
+
+    noFeed.forEach((user) => {
+      mySet.add(user.fromUserId.toString());
+      mySet.add(user.toUserId.toString());
+    });
+
+    //okay now i need to select all the users except the users whose object id is saved in mySet
+    const notInFeed = [...mySet];
+
+    const finalPeople = await User.find({
+      $and: [{ _id: { $nin: notInFeed } }, { _id: { $ne: loggedInUser._id } }],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "sent",
+      finalPeople,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+//no interaction virat,dhoni
 
 module.exports = userRouter;
